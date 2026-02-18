3963fb1d4e369a3016bc82abeec1a938d5ceae72
Simon Fowler <simon.fowler@anu.edu.au>
2026-02-18 12:57:24 +1100

    Fixes for clustercheck scripts.
    Newer haproxy is less forgiving about the HTTP implementation when using
    httpchk, so we need to do this properly.

--- files/clustercheck-maintenance.sh
+++ files/clustercheck-maintenance.sh
@@ -3,7 +3,10 @@
 # Return 503 when in maintenance
 
 /bin/echo -e "HTTP/1.1 503 Service Unavailable\r\n"
-/bin/echo -e "Content-Type: Content-Type: text/plain\r\n"
+/bin/echo -e "Content-Type: text/plain\r\n"
+/bin/echo -e "Content-Length: 18\r\n"
 /bin/echo -e "\r\n"
 /bin/echo -e "Node is *down*.\r\n"
 /bin/echo -e "\r\n"
+
+sleep 1
